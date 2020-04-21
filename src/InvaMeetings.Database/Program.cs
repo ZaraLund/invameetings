de6ed227d08c5a334043f7e4ad0aacdbad7c765b
Zara Lundgren <lundzas@gmail.com>
2020-04-21 13:31:02 +0200

    Add Docker config

--- src/InvaMeetings.Database/Program.cs
+++ src/InvaMeetings.Database/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using DbUp;
 
 namespace InvaMeetings.Database
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static bool IsDevelopment =>
+            Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development";
+        static int Main()
         {
-            Console.WriteLine("Hello World!");
+            var server = Environment.GetEnvironmentVariable("DB_SERVER");
+            var name = Environment.GetEnvironmentVariable("DB_NAME");
+            var user = Environment.GetEnvironmentVariable("DB_USER");
+            var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+            var connectionString = $"Server={server}; Database={name}; Uid={user}; Pwd={password}";
+            if (IsDevelopment)
+            {
+                EnsureDatabase.For.SqlDatabase(connectionString);
+            }
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithVariable("DatabaseName", name)
+                    .WithScriptsFromFileSystem("Scripts")
+                    .LogToConsole()
+                    .Build();
+            var result = upgrader.PerformUpgrade();
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+                if (IsDevelopment)
+                {
+                    Console.ReadLine();
+                }
+                return -1;
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
         }
     }
 }
---
+++ src/InvaMeetings.Database/Scripts/0000-UserTable.sql
@@ -0,0 +1,7 @@
+﻿USE [$DatabaseName$]
+CREATE TABLE [dbo].[User](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Firstname] [nvarchar](50) NOT NULL,
+	[Lastname] [nvarchar](50) NOT NULL
+) ON [PRIMARY]
+GO
