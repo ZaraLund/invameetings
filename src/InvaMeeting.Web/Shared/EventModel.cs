36509710a201805461bc56793d6f8da489db6056
Zara Lundgren <lundzas@gmail.com>
2020-04-27 18:39:19 +0200

    Add event form gui

---
+++ src/InvaMeeting.Web/Shared/EventModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvaMeetings.Web
+{
+    public class EventModel
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+        public DateTime? date { get; set; }
+        public string description { get; set; }
+
+        public EventModel() {
+            date = DateTime.Now;
+        }
+    }
+}
